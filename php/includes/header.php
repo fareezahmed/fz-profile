3e8377235991d114556cb2313f3e6e52ca551654
Fareez Ahmed <fareez1234@gmail.com>
2014-11-19 13:22:58 +1100

    Adding new data html and php code
    Adding all the php and html change of the project

---
+++ php/includes/header.php
@@ -0,0 +1,23 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Design You Desire - Fareez Ahmed</title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link id="scrollUpTheme" rel="stylesheet" href="css/image.css">
+    <link href="css/mainStyle.css" rel="stylesheet">
+    <link href="css/mediaQueryStyle.css" rel="stylesheet">
+
+    <link rel="icon" type="image/gif" href="images/logo-icon.gif" />
+
+    <link href='css/font1.css' rel='stylesheet' type='text/css'>
+    <link href='css/font2.css' rel='stylesheet' type='text/css'>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+  </head>
---
+++ php/includes/nav.php
@@ -0,0 +1,59 @@
+<header>
+       <div class="row">
+             <div class="col-md-1">
+            </div>
+            <div class="col-sm-5 col-xs-5 col-md-3 nav-logo-container no-padding-lr">
+              <ul class="nav nav-pills nav-logo">
+                <li> <a class="home" href="#">                   
+                    <h1> <img src="images/logo-sm.png" width="31" height="30" alt="Logo">Fareez Ahmed</h1>
+                  </a>
+                </li>
+              </ul>  
+            </div>
+            <div class="col-md-1 col-md-1">
+            </div>
+            <div class="col-md-5 col-sm-7 col-xs-7 nav-link-container no-padding">
+              <ul class="nav nav-pills pull-right">
+                <li> <a class="talents" href="#">
+                    Talents </span>
+                  </a>
+                </li>
+                <li> <a class="project" href="#">
+                    Projects </span>
+                  </a>
+                </li>
+                <li> <a class="consulting" href="#">
+                    Contact </span>
+                  </a>
+                </li>
+              </ul>  
+            </div>
+            <div class="col-xs-5 nav-logo-drop no-padding-lr">
+              <ul class="nav nav-pills nav-logo pull-right">
+                <li class="dropdown">
+                  <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                    <span class="glyphicon glyphicon-th-list"></span>
+                  </a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li> 
+                        <a class="talents" href="#">
+                          TALENTS
+                        </a>
+                    </li>
+                    <li> 
+                        <a class="project" href="#">
+                          PROJECTS
+                        </a>
+                    </li>
+                    <li> 
+                        <a class="con" href="#">
+                          CONTACT
+                        </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>  
+            </div>
+            <div class="col-md-1">
+            </div>
+       </div>
